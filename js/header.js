f4ab5a6d1230797410edce57e532bcf95677ff8b
Nuno Sousa <9180579@esmad.ipp.pt>
2021-04-21 19:47:23 +0100

    navabr fire

--- js/header.js
+++ js/header.js
@@ -1,4 +1,5 @@
 let header__hamburger = document.querySelector(".header__hamburger");
+let header = document.querySelector(".header");
 let main = document.querySelector(".main");
 let mobile_menu = document.querySelector(".mobile_menu");
 let mobile_menu__bg = document.querySelector(".mobile_menu__bg");
@@ -6,35 +7,46 @@ let mobile_menu__links = document.querySelector(".mobile_menu__links");
 let mobile_menu__services = document.querySelector(".mobile_menu__services");
 let mobile_menu__socials = document.querySelector(".mobile_menu__socials");
 
+let header_open = document.querySelector(".header_open");
+let header_open__cross = document.querySelector(".header_open__cross");
+
 header__hamburger.addEventListener("click", () => {
-  if (
-    !mobile_menu__links.classList.contains("open") &&
-    !mobile_menu__socials.classList.contains("open")
-  ) {
-    main.classList.add("no_scroll");
-    main.classList.add("menu-open");
+  main.classList.add("no_scroll");
+  main.classList.add("menu-open");
 
-    mobile_menu.classList.add("open");
-    mobile_menu__bg.classList.add("open");
+  mobile_menu.classList.add("open");
+  mobile_menu__bg.classList.add("open");
 
-    setTimeout(() => {
-      mobile_menu__links.classList.add("open");
-      mobile_menu__services.classList.add("open");
-      mobile_menu__socials.classList.add("open");
-    }, 600);
-  } else {
-    mobile_menu__links.classList.remove("open");
-    mobile_menu__services.classList.remove("open");
-    mobile_menu__socials.classList.remove("open");
+  setTimeout(() => {
+    header.classList.add("remove");
+  }, 150);
 
-    setTimeout(() => {
-      main.classList.remove("no_scroll");
-      main.classList.remove("menu-open");
-    }, 800);
+  setTimeout(() => {
+    header_open.classList.add("open");
 
-    setTimeout(() => {
-      mobile_menu.classList.remove("open");
-      mobile_menu__bg.classList.remove("open");
-    }, 600);
-  }
+    mobile_menu__links.classList.add("open");
+    mobile_menu__services.classList.add("open");
+    mobile_menu__socials.classList.add("open");
+  }, 600);
+});
+
+header_open__cross.addEventListener("click", () => {
+  mobile_menu__links.classList.remove("open");
+  mobile_menu__services.classList.remove("open");
+  mobile_menu__socials.classList.remove("open");
+
+  setTimeout(() => {
+    header_open.classList.remove("open");
+  }, 150);
+
+  setTimeout(() => {
+    mobile_menu.classList.remove("open");
+    mobile_menu__bg.classList.remove("open");
+  }, 600);
+
+  setTimeout(() => {
+    main.classList.remove("no_scroll");
+    main.classList.remove("menu-open");
+    header.classList.remove("remove");
+  }, 800);
 });
