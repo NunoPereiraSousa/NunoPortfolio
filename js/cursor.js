a9faf5cc90e0e88de48ce6a677b7804585d2ce4e
Nuno Sousa <9180579@esmad.ipp.pt>
2021-04-24 01:30:53 +0100

    Desktop, about me and cursor adjusted

--- js/cursor.js
+++ js/cursor.js
@@ -1,6 +1,7 @@
 // CURSOR
 let cursor = $(".cursor"),
-  follower = $(".cursor-follower");
+  follower = $(".cursor-follower"),
+  contacts = $(".contacts");
 
 let posX = 0,
   posY = 0;
@@ -16,8 +17,8 @@ TweenMax.to({}, 0.016, {
 
     TweenMax.set(follower, {
       css: {
-        left: posX - 12,
-        top: posY - 12
+        left: posX - follower.width() / 2,
+        top: posY - follower.height() / 2
       }
     });
 
@@ -33,4 +34,22 @@ TweenMax.to({}, 0.016, {
 $(document).on("mousemove", e => {
   mouseX = e.clientX;
   mouseY = e.clientY;
+});
+
+$(".contacts").on("mouseenter", () => {
+  cursor.addClass("active");
+  follower.addClass("active");
+});
+$(".contacts").on("mouseleave", () => {
+  cursor.removeClass("active");
+  follower.removeClass("active");
+});
+
+$(".open_work").on("mouseenter", () => {
+  cursor.addClass("open_work_cursor");
+  follower.addClass("open_work_cursor");
+});
+$(".open_work").on("mouseleave", () => {
+  cursor.removeClass("open_work_cursor");
+  follower.removeClass("open_work_cursor");
 });
