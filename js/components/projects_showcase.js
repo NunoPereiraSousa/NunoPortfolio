3024641542f0058d3849dde7cb1667f92b05065f
Nuno Sousa <9180579@esmad.ipp.pt>
2021-04-29 00:27:07 +0100

    Projects UI upgraded

--- js/components/projects_showcase.js
+++ js/components/projects_showcase.js
@@ -1,6 +1,7 @@
 import { projectsShowcase } from "../models/projects_showcase.js";
 
 let projects_showcase = document.querySelector(".projects_showcase__grid");
+let projects_showcase2 = document.querySelector(".projects_showcase__grid2");
 
 for (let project of projectsShowcase) {
   projects_showcase.innerHTML += `
@@ -9,4 +10,10 @@ for (let project of projectsShowcase) {
         <a href="${project.url}">${project.title} <span>${project.arrow}</span></a>
     </div>
     `;
+  projects_showcase2.innerHTML += `
+      <div class="projects_showcase__grid__card flex flex-ai-fe">
+          <h1>${project.id}</h1>
+          <a href="${project.url}">${project.title} <span>${project.arrow}</span></a>
+      </div>
+      `;
 }
